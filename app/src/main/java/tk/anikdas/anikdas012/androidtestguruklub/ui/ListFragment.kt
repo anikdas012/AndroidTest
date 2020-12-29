f6f44535b3ae6e139020d178fdce0e9e3a3a3caa
Anik Das <anikdas012@gmail.com>
2020-12-30 01:53:13 +0600

    Create new fragment for showing list

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/ui/ListFragment.kt
@@ -0,0 +1,23 @@
+package tk.anikdas.anikdas012.androidtestguruklub.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by "Anik Das" on 30-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class ListFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
