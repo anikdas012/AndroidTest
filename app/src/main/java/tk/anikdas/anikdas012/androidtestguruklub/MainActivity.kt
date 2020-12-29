cc949e5d2fcb698a03566a954a7a13fc92728ea4
Anik Das <anikdas012@gmail.com>
2020-12-30 01:59:11 +0600

    Add log statement

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/MainActivity.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/MainActivity.kt
@@ -2,10 +2,17 @@ package tk.anikdas.anikdas012.androidtestguruklub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import tk.anikdas.anikdas012.androidtestguruklub.ui.ListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: called")
     }
 }
