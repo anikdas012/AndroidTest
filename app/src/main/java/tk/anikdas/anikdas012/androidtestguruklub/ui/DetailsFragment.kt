b52e1583b6ac1454e1b8bcd52ac59880fc62c38d
Anik Das <anikdas012@gmail.com>
2020-12-30 23:55:41 +0600

    Add new fragment for details screen

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/ui/DetailsFragment.kt
@@ -0,0 +1,11 @@
+package tk.anikdas.anikdas012.androidtestguruklub.ui
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by "Anik Das" on 30-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class DetailsFragment : Fragment() {
+}
