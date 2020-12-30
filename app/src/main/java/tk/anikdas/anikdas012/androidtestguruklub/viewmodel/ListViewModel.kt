53b0e606fce168d4618f7d2baf84c1fc1906d177
Anik Das <anikdas012@gmail.com>
2020-12-30 18:40:30 +0600

    Add new method to get list

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/ListViewModel.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/ListViewModel.kt
@@ -1,6 +1,9 @@
 package tk.anikdas.anikdas012.androidtestguruklub.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import tk.anikdas.anikdas012.androidtestguruklub.model.ListModel
 
 /**
  * Created by "Anik Das" on 30-Dec-2020
@@ -10,4 +13,8 @@ import androidx.lifecycle.ViewModel
 class ListViewModel: ViewModel() {
 
     private val TAG = "ListViewModel"
+
+    fun getList(): LiveData<List<ListModel>> {
+        Log.d(TAG, "getList: called")
+    }
 }
