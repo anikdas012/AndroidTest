939e0d8011c80ff618602bdf1bc1fccb34690e08
Anik Das <anikdas012@gmail.com>
2020-12-30 23:43:33 +0600

    Add interface to navigate fragments on recyclerview item click

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/utils/ItemClickListener.kt
@@ -0,0 +1,10 @@
+package tk.anikdas.anikdas012.androidtestguruklub.utils
+
+/**
+ * Created by "Anik Das" on 30-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+interface ItemClickListener {
+    fun onItemClick(id: Int)
+}
