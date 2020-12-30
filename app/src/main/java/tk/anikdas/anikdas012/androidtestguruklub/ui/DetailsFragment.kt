07059c25c0ef1dbb02d91736bca2c584152fb003
Anik Das <anikdas012@gmail.com>
2020-12-31 00:09:12 +0600

    Return view

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/ui/DetailsFragment.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/ui/DetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tk.anikdas.anikdas012.androidtestguruklub.databinding.FragmentDetailsBinding
 
 /**
  * Created by "Anik Das" on 30-Dec-2020
@@ -14,12 +15,15 @@ import androidx.fragment.app.Fragment
 class DetailsFragment : Fragment() {
 
     private val TAG = "DetailsFragment"
+    private lateinit var binding: FragmentDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
