8719e714735e1c158bf4b813d7ba0d391580f67d
Anik Das <anikdas012@gmail.com>
2020-12-30 18:54:19 +0600

    Create adapter for recyclerview

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/ui/ListAdapter.kt
@@ -0,0 +1,29 @@
+package tk.anikdas.anikdas012.androidtestguruklub.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by "Anik Das" on 30-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
