4e4d8f20b809c30fc60b896215ff5861dc09fb78
Anik Das <anikdas012@gmail.com>
2020-12-30 17:55:44 +0600

    Add method to create retrofit instance in a singleton manner

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/RetrofitServiece.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/RetrofitServiece.kt
@@ -1,9 +1,28 @@
 package tk.anikdas.anikdas012.androidtestguruklub.network
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * Created by "Anik Das" on 30-Dec-2020
  * Developer email: "anikdas012@gmail.com"
  */
 
 class RetrofitServiece {
+
+    companion object {
+        private var retrofit: Retrofit? = null
+
+        private fun getRetrofitInstance() : Retrofit {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                        .baseUrl("http://api.tvmaze.com")
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+            }
+            return retrofit!!
+        }
+    }
 }
