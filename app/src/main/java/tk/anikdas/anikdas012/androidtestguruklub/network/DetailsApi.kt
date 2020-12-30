901c9fb78d335bc10868aee1f5b03b312cf2f3e6
Anik Das <anikdas012@gmail.com>
2020-12-30 18:14:25 +0600

    Add interface to get details

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/DetailsApi.kt
@@ -0,0 +1,9 @@
+package tk.anikdas.anikdas012.androidtestguruklub.network
+
+/**
+ * Created by "Anik Das" on 30-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+interface DetailsApi {
+}
