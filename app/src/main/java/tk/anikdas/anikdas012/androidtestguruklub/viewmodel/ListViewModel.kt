022eae4950374fbeb164e18553e665549001b66a
Anik Das <anikdas012@gmail.com>
2020-12-30 23:16:32 +0600

    Request data from api and pass to view as livedata

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/ListViewModel.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/ListViewModel.kt
@@ -2,8 +2,12 @@ package tk.anikdas.anikdas012.androidtestguruklub.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
+import io.reactivex.schedulers.Schedulers
+import tk.anikdas.anikdas012.androidtestguruklub.model.Image
 import tk.anikdas.anikdas012.androidtestguruklub.model.ListModel
+import tk.anikdas.anikdas012.androidtestguruklub.network.RetrofitServiece
 
 /**
  * Created by "Anik Das" on 30-Dec-2020
@@ -16,5 +20,15 @@ class ListViewModel: ViewModel() {
 
     fun getList(): LiveData<List<ListModel>> {
         Log.d(TAG, "getList: called")
+        return LiveDataReactiveStreams.fromPublisher(
+            RetrofitServiece.getListApi().list()
+                .onErrorReturn {
+                    Log.d(TAG, "getList: error")
+                    return@onErrorReturn listOf<ListModel>(
+                        ListModel(-1, "Error", "Error", listOf("Error"), Image("Error", "Error"))
+                    )
+                }
+                .subscribeOn(Schedulers.io())
+        )
     }
 }
