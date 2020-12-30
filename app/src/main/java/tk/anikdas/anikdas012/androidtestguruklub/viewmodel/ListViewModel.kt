b47c59538c3f685fc08662fef6934ec58cf8af4d
Anik Das <anikdas012@gmail.com>
2020-12-30 18:36:27 +0600

    Create new class for ListViewModel

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/ListViewModel.kt
@@ -0,0 +1,13 @@
+package tk.anikdas.anikdas012.androidtestguruklub.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by "Anik Das" on 30-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class ListViewModel: ViewModel() {
+
+    private val TAG = "ListViewModel"
+}
