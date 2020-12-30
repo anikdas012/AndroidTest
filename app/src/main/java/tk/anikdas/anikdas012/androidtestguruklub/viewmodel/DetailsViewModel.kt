8c72ac84a60e10cc14c8ea2126e04c900ee4d486
Anik Das <anikdas012@gmail.com>
2020-12-31 01:57:37 +0600

    Add method to retrive and send data from api to view

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/DetailsViewModel.kt
@@ -1,6 +1,13 @@
 package tk.anikdas.anikdas012.androidtestguruklub.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
+import io.reactivex.schedulers.Schedulers
+import tk.anikdas.anikdas012.androidtestguruklub.model.DetailsModel
+import tk.anikdas.anikdas012.androidtestguruklub.model.Image
+import tk.anikdas.anikdas012.androidtestguruklub.network.RetrofitServiece
 
 /**
  * Created by "Anik Das" on 31-Dec-2020
@@ -10,4 +17,21 @@ import androidx.lifecycle.ViewModel
 class DetailsViewModel : ViewModel() {
 
     private val TAG = "DetailsViewModel"
+
+    fun getDetails(id: Int) : LiveData<DetailsModel> {
+        Log.d(TAG, "getDetails: called")
+        return LiveDataReactiveStreams.fromPublisher(
+            RetrofitServiece.getDetailsApi().details(id)
+                .onErrorReturn {
+                    return@onErrorReturn DetailsModel("Error",
+                        "Error",
+                    "Error",
+                             listOf("Error"),
+                    "Error",
+                             Image("Error", "Error")
+                    )
+                }
+                .subscribeOn(Schedulers.io())
+        )
+    }
 }
