15c69045f17c4d26f866ebfb02c13f4a2ad01013
Anik Das <anikdas012@gmail.com>
2020-12-30 18:19:44 +0600

    Add method to get details response

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/DetailsApi.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/DetailsApi.kt
@@ -1,9 +1,16 @@
 package tk.anikdas.anikdas012.androidtestguruklub.network
 
+import io.reactivex.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 /**
  * Created by "Anik Das" on 30-Dec-2020
  * Developer email: "anikdas012@gmail.com"
  */
 
 interface DetailsApi {
+
+    @GET("/{id}")
+    fun details(@Path("id") id: Int): Flowable<DetailsApi>
 }
