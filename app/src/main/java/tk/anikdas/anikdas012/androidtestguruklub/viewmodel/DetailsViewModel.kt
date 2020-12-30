06a7b166dcf24620991bffa65ca314fd57f9aefa
Anik Das <anikdas012@gmail.com>
2020-12-31 01:38:56 +0600

    Add new class for DetailsViewModel

---
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/viewmodel/DetailsViewModel.kt
@@ -0,0 +1,13 @@
+package tk.anikdas.anikdas012.androidtestguruklub.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by "Anik Das" on 31-Dec-2020
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class DetailsViewModel : ViewModel() {
+
+    private val TAG = "DetailsViewModel"
+}
