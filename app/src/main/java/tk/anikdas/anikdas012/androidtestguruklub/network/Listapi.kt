23bee6558401da8965e0183ad3df729b24738682
Anik Das <anikdas012@gmail.com>
2020-12-30 18:13:05 +0600

    Add method to get list

--- app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/Listapi.kt
+++ app/src/main/java/tk/anikdas/anikdas012/androidtestguruklub/network/Listapi.kt
@@ -1,9 +1,16 @@
 package tk.anikdas.anikdas012.androidtestguruklub.network
 
+import io.reactivex.Flowable
+import retrofit2.http.GET
+import tk.anikdas.anikdas012.androidtestguruklub.model.ListModel
+
 /**
  * Created by "Anik Das" on 30-Dec-2020
  * Developer email: "anikdas012@gmail.com"
  */
 
 interface Listapi {
+
+    @GET("/shows")
+    fun list(): Flowable<List<ListModel>>
 }
